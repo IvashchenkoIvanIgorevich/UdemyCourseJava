90b4fb1943ee7ede8646028625873b947129073d
IvashchenkoIvanIgorevich <ivachko2009@gmail.com>
2022-05-18 16:03:25 +0300

    Done challenge SecondsAndMinutesChallenge

---
+++ Challenges/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println(SecondsAndMinutesChallenge.getDurationString(6530));
+        System.out.println(SecondsAndMinutesChallenge.getDurationString(108, 50));
+    }
+}
---
+++ Challenges/src/SecondsAndMinutesChallenge.java
@@ -0,0 +1,22 @@
+public class SecondsAndMinutesChallenge {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+    public static String getDurationString(int minutes, int seconds){
+        if (minutes < 0 || seconds < 0 || seconds > 59)
+            return INVALID_VALUE_MESSAGE;
+
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+
+    public static String  getDurationString(int seconds){
+        if (seconds < 0)
+            return INVALID_VALUE_MESSAGE;
+
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
+    }
+}
