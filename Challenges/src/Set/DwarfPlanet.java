4c79f4e6835d00bb2dd5fc3ca0c3785e3ef8119b
IvashchenkoIvanIgorevich <ivachko2009@gmail.com>
2022-07-10 11:32:03 +0300

    Dealing with Set interface and methods equals and hashCode

---
+++ Challenges/src/Set/DwarfPlanet.java
@@ -0,0 +1,7 @@
+package Set;
+
+public class DwarfPlanet  extends HeavenlyBody{
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
---
+++ Challenges/src/Set/Moon.java
@@ -0,0 +1,7 @@
+package Set;
+
+public class Moon extends HeavenlyBody{
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
